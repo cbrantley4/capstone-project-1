52c769987a0b4d0db53faa24f49b6bfbcd9b5fde
Cameron Brantley <brantley.cameron@gmail.com>
2020-03-29 11:08:18 -0500

    Set up email

--- components/views/contact.js
+++ components/views/contact.js
@@ -5,7 +5,6 @@ export default () =>
   <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ8Z-LNMMOF_Ir9sUCUiHEKnyX76n8XjPwoJALf0_Qb26gPGkBw">
   <p>My name is Cameron Brantley. I created this page to help you find a recreational trail in the city where you are traveling. Please leave me a comment about this page.</p>
   <div>
-  <textarea id="email" name="email" rows=10 cols = 120>Leave your email address if you would like a response. </textarea>
-  <button>Send</button>
+  <button><a href="mailto:brantley.cameron@gmail.com" target="_blank">Email Me</a></button>
   </div>
 </section>`;
