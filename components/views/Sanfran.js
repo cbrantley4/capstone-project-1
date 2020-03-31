124d28ebb4238cc62e59b7fd86d6f8298b4269e6
Cameron Brantley <brantley.cameron@gmail.com>
2020-03-30 20:19:49 -0500

    Reformatted API call

--- components/views/Sanfran.js
+++ components/views/Sanfran.js
@@ -1,5 +1,5 @@
 export default st => `
-<section id="SanFran">
+<section id="Sanfran">
     <nav>
       <img src = "https://images.unsplash.com/photo-1422226256160-9b266e308ea6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60">
         <ul>
